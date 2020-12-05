5a0cd21529bcd8e909d88b9741ad1067acd2f497
Uladzislau Volchyk <uladzislau.volchyk@gmail.com>
2020-12-05 11:53:33 +0300

    feat: add test case #4

--- lab7/src/test/java/EbayTest.java
+++ lab7/src/test/java/EbayTest.java
@@ -2,9 +2,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -12,22 +9,17 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class EbayTests {
+public class EbayTest {
 
     private WebDriver driver;
-    private WebDriverWait wait;
 
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
-        driver.get("https://ebay.com");
         driver.manage().window().maximize();
+        driver.get("https://ebay.com");
 
-        wait = new WebDriverWait(driver, 20);
-
-        WebElement languageButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@id=\"gh-eb-Geo-a-default\"]")));
-        Actions actions = new Actions(driver);
-        actions.click().perform();
+        WebElement languageButton = clickableElement(By.xpath("//a[@id='gh-eb-Geo-a-default']"));
         if (!languageButton.getText().toLowerCase().contains("english")) {
             languageButton
                     .click();
@@ -37,22 +29,29 @@ public class EbayTests {
         }
     }
 
+    @Test
+    public void testResultsAreCorrectForANonExistingItem() {
+        visibleElementBy(By.xpath("//*[@id='gh-ac-box2']/input[@type='text']"))
+                .sendKeys("dfsfhjdkalfkdnglf#$!@)(_!");
+        clickableElement(By.xpath("//*[@id='gh-btn']"))
+                .click();
+        WebElement resultBox = visibleElementBy(By.xpath("//*[@id=\"srp-river-results\"]"));
+        Assert.assertTrue(resultBox.getText().contains("No exact matches found"));
+    }
+
+    private WebElement clickableElement(By locator) {
+        return new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    private WebElement visibleElementBy(By locator) {
+        return new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
     @AfterMethod
     public void tearDown() {
         driver.quit();
         driver = null;
-        wait = null;
-    }
-
-    @Test
-    public void testResultsAreCorrectForANonExistingItem() {
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='gh-ac-box2']/input[@type='text']")))
-                .sendKeys("dfsfhjdkalfkdnglf#$!@)(_!");
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='gh-btn']")))
-                .click();
-
-        WebElement resultBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"srp-river-results\"]")));
-        Assert.assertTrue(resultBox.getText().contains("No exact matches found"));
     }
 }
