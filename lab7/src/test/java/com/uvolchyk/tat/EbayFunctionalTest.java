427ea9d49c176b2d489594bfc6183ddeec0484d7
Uladzislau Volchyk <uladzislau.volchyk@gmail.com>
2020-12-05 17:53:59 +0300

    refactor: add options for non-sandbox launch

--- lab7/src/test/java/com/uvolchyk/tat/EbayFunctionalTest.java
+++ lab7/src/test/java/com/uvolchyk/tat/EbayFunctionalTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -14,18 +15,21 @@ import org.testng.annotations.Test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class FilterSearchResultTest {
-
-    private final Double lowerBound = 0.0;
-    private final Double upperBound = 5.0;
+public class EbayFunctionalTest {
 
     private final Integer timeOutDuration = 10;
     private WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://ebay.com");
 
@@ -38,21 +42,45 @@ public class FilterSearchResultTest {
     }
 
     @Test
+    public void testItemsInSearchListAreSortedByMaxPrice() {
+        visibleElement(By.xpath("//*[@id='gh-ac-box2']/input[@type='text']"))
+                .sendKeys("comics");
+        clickableElement(By.xpath("//*[@id='gh-btn']"))
+                .click();
+        clickableElement(By.xpath("//button[@aria-label='Sort selector. Best Match selected.']"))
+                .click();
+        clickableElement(By.xpath("//a[contains(., 'Price + Shipping: highest first')]"))
+                .click();
+
+        List<Integer> sortedByMaxPriceItems = visibleElements(By.xpath("//*[@id='srp-river-results']/ul/li")).stream()
+                .map(item -> item.findElement(By.xpath("//span[@class='s-item__price']"))
+                        .getText().replaceAll("[^0-9]", ""))
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
+
+        Assert.assertTrue(IntStream.range(0, sortedByMaxPriceItems.size() - 1)
+                .noneMatch(i -> sortedByMaxPriceItems.get(i) > sortedByMaxPriceItems.get(i + 1)));
+    }
+
+    @Test
     public void testSearchResultItemsAreFiltered() {
-        visibleElementBy(By.xpath("//*[@id='gh-ac-box2']/input[@type='text']"))
+        final Double lowerBound = 0.0;
+        final Double upperBound = 5.0;
+
+        visibleElement(By.xpath("//*[@id='gh-ac-box2']/input[@type='text']"))
                 .sendKeys("pie");
         clickableElement(By.xpath("//*[@id='gh-btn']"))
                 .click();
 
-        visibleElementBy(By.xpath("//input[@aria-label='Minimum Value in $']"))
+        visibleElement(By.xpath("//input[@aria-label='Minimum Value in $']"))
                 .sendKeys("0");
-        visibleElementBy(By.xpath("//input[@aria-label='Maximum Value in $']"))
+        visibleElement(By.xpath("//input[@aria-label='Maximum Value in $']"))
                 .sendKeys("5");
         clickableElement(By.xpath("//button[@aria-label='Submit price range']"))
                 .click();
 
         List<Double> filteredByPriceItems =
-                visibleElements(By.xpath("//*[@id=\"srp-river-results\"]/ul//span[@class='s-item__price']")).stream()
+                visibleElements(By.xpath("//*[@id='srp-river-results']/ul//span[@class='s-item__price']")).stream()
                         .map(item -> item.getText().split("to"))
                         .map(i ->
                                 Arrays.stream(i)
@@ -67,6 +95,16 @@ public class FilterSearchResultTest {
                 .noneMatch(price -> price > upperBound && price < lowerBound));
     }
 
+    @Test
+    public void testNonExistingItemMessageShow() {
+        visibleElement(By.xpath("//*[@id='gh-ac-box2']/input[@type='text']"))
+                .sendKeys("dfsfhjdkalfkdnglf#$!@)(_!");
+        clickableElement(By.xpath("//*[@id='gh-btn']"))
+                .click();
+        WebElement resultBox = visibleElement(By.xpath("//*[@id='srp-river-results']"));
+        Assert.assertTrue(resultBox.getText().contains("No exact matches found"));
+    }
+
     @AfterMethod
     public void tearDown() {
         driver.quit();
@@ -78,7 +116,7 @@ public class FilterSearchResultTest {
                 .until(ExpectedConditions.elementToBeClickable(locator));
     }
 
-    private WebElement visibleElementBy(By locator) {
+    private WebElement visibleElement(By locator) {
         return new WebDriverWait(driver, timeOutDuration)
                 .until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
